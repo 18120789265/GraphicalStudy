54ff0d2e51b79d2070c82023415c67d6c3cc210c
Stone <827443469@qq.com>
2017-11-28 21:18:03 +0800

    c++ peimer 第二章 引用 指针 常量

--- Primary/CAddAdd/BeginCCC/BeginCCC/BeginCCC.cpp
+++ Primary/CAddAdd/BeginCCC/BeginCCC/BeginCCC.cpp
@@ -4,6 +4,8 @@
 #include <math.h>
 using namespace std;
 
+void test3(int *a);
+void test(int &a);
 void test2();
 void print(intptr_t i);
 
@@ -44,9 +46,55 @@ void test2() {
 	/*cout << "2M" << endl;
 	cout << "2\tM\n";*/
 	//变量
-	string book("this is a book");
+	//string book("this is a book");
 	//cout << book << endl;
+	/*char v = u'1';
+	cout << v << endl;*/
+	/*int _ = 1;
+	print(_);*/
+	//引用，非单指类的引用,引用一旦建立不可修改
+	//int a = 1;
+	//int &b = a;//指向a的地址
+	//b = 2;//a = 2
+	//cout << a << endl;//输出了a的地址
+	//int &c = b;
+	//c = 3;//a = 3
+	//print(a);
+	/*int a = 1;
+	test(a);
+	cout << a << endl;*/
+	//指针，指针本身就是一个对象，可对指针进行拷贝修改
+	//int a = 1;
+	//int b = 2;
+	//int *c = &a;//初始化指针C并赋值
+	//c = &b;//改变c指向的值
+	//cout << *c << endl;//输出c指向的值
+	//cout << c << endl;//输出c的地址
+	//int &d = a;
+	//c = &d;
+	//cout << c << endl;//输出a的地址
+	/*cout << a << endl;
+	test3(b);
+	cout << a << endl;*/
+	/*int a = 2;
+	int *b = &a;
+	*b = *b * *b;
+	cout << a << endl;*/
+	int a = 1;
+	int *b = &a;
+	int &c = *b;//== int *&c = b;
+	cout << *b;
 }
+
+//引用传参
+void test(int &a) {
+	a = 3;
+}
+//指针传参
+void test3(int *a) {
+	*a = 3;
+}
+
 
 //带换行的输出
 void print(intptr_t i) {
